1ca96076110b6bd8a81a079941f1fca0a42407b0
Liam <liamfpower@gmail.com>
2020-01-17 13:18:53 +1100

    initial commit

---
+++ js/buttons.js
@@ -0,0 +1,53 @@
+cnt = document.querySelector('.container');
+
+flexDirection = document.querySelector('.flexdirection')
+justifyContent = document.querySelector('.justifycontent')
+alignItems = document.querySelector('.alignitems');
+flexWrap = document.querySelector('.flexwrap');
+alignContent = document.querySelector('.aligncontent');
+randomSizes = document.querySelector('.randomsizes');
+
+let i = 0;
+
+
+const stepThroughOpts = (button, property, opts) => {
+    i = (i + 1) % opts.length;
+    cnt.style[property] = opts[i];
+    button.innerHTML = `${property}: ${opts[i]}`;
+}
+
+
+flexDirection.addEventListener('click', () => {
+    const directions = ["row", "column", "column-reverse", "row-reverse"];
+    stepThroughOpts(flexDirection, 'flex-direction', directions)
+
+})
+
+justifyContent.addEventListener('click', () => {
+    const options = ["start", "center", "space-between", "space-around", "space-evenly", "flex-start", "flex-end", "left", "right", "normal"];
+    stepThroughOpts(justifyContent, 'justify-content', options)
+
+});
+
+alignItems.addEventListener('click', () => {
+    const options = ["stretch", "center", "start", "end", "flex-start", "flex-end"];
+    stepThroughOpts(alignItems, 'align-items', options)
+
+});
+
+flexWrap.addEventListener('click', () => {
+    const options = ["nowrap", "wrap", "wrap-reverse"];
+    stepThroughOpts(flexWrap, 'flex-wrap', options);
+});
+
+alignContent.addEventListener('click', () => {
+    const options = ["start", "center", "space-between", "space-around"];
+    stepThroughOpts(alignContent, 'align-content', options);
+});
+
+
+randomSizes.addEventListener('click', () => {
+    boxArray.forEach((box) => {
+        randomSize(box, 100, 300);
+    })
+})
---
+++ js/script.js
@@ -0,0 +1,73 @@
+const makeFlexItems = (num) => {
+    for(let i = 0; i < num; i++) {
+        let container = document.querySelector('.container')
+        let div = document.createElement('div');
+        let p = document.createElement('p');
+        div.classList.add('flex-item');
+        div.appendChild(p);
+        container.appendChild(div);
+    }
+}
+
+const initBoxes = (num) => {
+    if(document.querySelector('flex-item')) {
+        removeBoxes();
+    }
+    makeFlexItems(num);
+    const flexBoxes = document.querySelectorAll('.flex-item');
+    return Array.prototype.slice.call(flexBoxes);
+
+}
+
+boxArray = initBoxes(4);
+
+
+const makeRandomColor = () => {
+
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+
+    return `rgb(${r},${g},${b})`
+}
+
+
+const randRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+}
+
+const randomSize = (element, min, max) => {
+    element.style.width = `${randRange(min, max)}px`;
+    element.style.height = `${randRange(min, max)}px`;
+}
+
+
+
+const changeColor = (element, color) => {
+    element.style.backgroundColor = color;
+}
+
+
+boxArray.forEach((box, i) => {
+
+    changeColor(box, makeRandomColor());
+
+
+    const colourArray = box.style.backgroundColor.split("(")[1].split(")")[0].split(",")
+    const nums = colourArray.map(num => parseInt(num, 10));
+    const colouravg = nums.reduce((lastnum, num) => lastnum + num, 0);
+    if(colouravg > 450) {
+        box.firstElementChild.style.color = "black";
+    } else {
+        box.firstElementChild.style.color = "white";
+    }
+    box.firstElementChild.innerHTML = i + 1;
+})
+
+
+const removeBoxes = () => {
+    boxArray.forEach((box) => {
+        console.log(box)
+        cnt.removeChild(box)
+    })
+}
